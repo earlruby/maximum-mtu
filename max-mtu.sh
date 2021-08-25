279513f66007dd398043a94907489b7651f2727d
eruby <eruby@vmware.com>
2021-08-25 16:51:08 -0700

    Added isupport for BSD ping and more error checking

--- max-mtu.sh
+++ max-mtu.sh
@@ -5,9 +5,28 @@
 
 # Usage: max-mtu.sh $target_host
 
+if ! which -s ping; then
+    echo "ping is not installed"
+    exit 1
+fi
+
 target_host=$1
 size=1272
-while ping -s $size -M do -c1 $target_host >&/dev/null; do
+
+if ! ping -c1 $target_host >&/dev/null; then
+   echo "$target_host does not respond to ping"
+   exit 1
+fi
+
+if ping -s $size -M do -c1 $target_host >&/dev/null; then
+   # GNU ping
+   nofragment='-M do'
+else
+   # BSD ping
+   nofragment='-D'
+fi
+
+while ping -s $size $nofragment -c1 $target_host >&/dev/null; do
     ((size+=4));
 done
 echo "Max MTU size to $target_host: $((size-4+28))"
